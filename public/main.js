5545a2b3fcc5716020e5fb74854db447f1494c7f
DawiT <Dawid@Tararuj.be>
2020-12-29 10:34:00 +0100

    exo 2 start

---
+++ public/main.js
@@ -0,0 +1,55 @@
+let phrase = `Bonjour tout le monde      `; 
+
+//long de la ph
+console.log(phrase.length);
+
+
+// sans espaces ; 
+phrase = phrase.trim(); 
+
+console.log(phrase.length);
+
+// uniquemenet la -1 lettre 
+let nouvellePhrase = phrase.substr(phrase.length-1, 1);
+
+console.log(nouvellePhrase);
+
+
+// sans quel mot? 
+let sans = "Bonjour "; 
+
+let sanssalut = phrase.replace(sans, ''); 
+
+console.log(sanssalut); 
+
+
+// avec le mot ; 
+let avecsalut = sans.concat(sanssalut);
+
+console.log(avecsalut);
+
+// supp la ph sauf: les 3 sont ok ; ; 
+
+let nouvelleph2 = phrase.substr(0, sans.length);
+console.log(nouvelleph2);
+
+//let nouvelleph2 = phrase.slice(0, sans.length);
+//console.log(nouvelleph2);
+
+//let nouvelleph2 = phrase.split('Bonjour');  
+//console.log(nouvelleph2[1]);
+
+
+
+// exo 2 ; 
+let phph2 = "Ceci est une variable de type number"; 
+
+phph2 = phph2.replace('number','string');
+console.log(phph2);
+
+
+
+
+
+
+
